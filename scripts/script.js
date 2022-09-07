a8819abed6f53e6fc4a1a95d28a59850bd3c5831
selma <sam.learntocode@gmail.com>
2022-09-07 16:56:07 +0100

    Add a function that displays the digit you press
    It's a rough and hacky implementation, just to see how it can
    be done

--- scripts/script.js
+++ scripts/script.js
@@ -65,3 +65,24 @@ function operate(operator, a, b){
             console.log('something went wrong');
     }
 }
+
+// get value from button
+document.querySelectorAll('.digit').forEach(button => 
+    button.addEventListener('click', e => {
+        displayValue(e.target);
+
+        let value = document.getElementById('screen-container').textContent;
+        console.log(value);
+    }));
+      
+// display that value
+function displayValue(button){
+    let value = button.textContent;
+    let screen = document.getElementById('screen-container');
+
+    if(screen.textContent === ''){
+        screen.innerText = value;
+    } else {
+        screen.innerText += value;
+    }
+}
