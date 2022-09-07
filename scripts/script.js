c4ad80ed1bb5e7845209b6d16b8d1afc5e659fb9
selma <sam.learntocode@gmail.com>
2022-09-07 16:09:19 +0100

    Implement basic calculator functions

---
+++ scripts/script.js
@@ -0,0 +1,44 @@
+function add(a, b){
+    a = Number(a);
+    b = Number(b);
+
+    if(Number.isNaN(a) || Number.isNaN(b))
+        return 'ERROR';
+
+    return a + b;
+}
+
+function subtract(a, b){
+    a = Number(a);
+    b = Number(b);
+
+    if(Number.isNaN(a) || Number.isNaN(b))
+        return 'ERROR';
+
+    return a - b;
+}
+
+function multiply(a, b){
+    a = Number(a);
+    b = Number(b);
+
+    if(Number.isNaN(a) || Number.isNaN(b))
+        return 'ERROR';
+
+    return a * b;
+}
+
+function divide(a, b){
+    a = Number(a);
+    b = Number(b);
+
+    if(Number.isNaN(a) || Number.isNaN(b))
+        return 'ERROR';
+    else {
+        if(b === 0){
+            return 'ERROR';
+        }
+
+        return a / b;
+    }
+}
